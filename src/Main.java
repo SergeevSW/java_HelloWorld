cf7cfe1694e1eca967e8c78f97e773c9e12da107
Сергеев Сергей <ssergeev@diasoft.ru>
2016-03-02 15:32:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+//        System.out.println();
+//        double aaa = 0x0.Fp3;
+        double aaa = 0x1.Fp10;
+        System.out.println(aaa);
+    }
+
+}
+
